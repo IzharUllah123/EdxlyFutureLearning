7d0c434cf28ca906f1a713976c7443630fa7f570
Izhar Ullah <ixharkhan9@gmail.com>
2026-03-06 22:38:46 +0500

    Update the capictor Config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.Edxly_FutureLearning',
   appName: 'Edxly_FutureLearning',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https' // Ensure this matches your error screenshot
+  }
 };
 
 export default config;
